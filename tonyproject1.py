a0b79abbca4cc46691805a5c44367f004a918b99
tonyrk-23 <tonyrohitrk@gmail.com>
2025-01-23 10:21:51 +0530

    Create tonyproject1.py

---
+++ tonyproject1.py
@@ -0,0 +1,9 @@
+# Taking input for two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Adding the numbers
+sum = num1 + num2
+
+# Displaying the result
+print("The sum of", num1, "and", num2, "is", sum)
